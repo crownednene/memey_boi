598276d326aacb12ba7d1e1bdc945e8326e574e0
Hanae El Mansouri <43448355+crownednene@users.noreply.github.com>
2020-06-18 12:30:35 -0700

    console printing

--- memey_boi.py
+++ memey_boi.py
@@ -12,14 +12,17 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-	print('we have logged in as {0.user}'.format(client))
+	print('We have logged in as {0.user}.'.format(client))
 
 @client.event
 async def on_message(message):
+	print(f"{message.channel}: {message.author}: {message.author.name}: {message.content}")
+
 	if message.author == client.user:
 		return
 
 	if message.content.startswith('$hello'):
 		await message.channel.send('fuck off')
+
 
 client.run(TOKEN)
