efd93f41926686f8f8d8b7f42728dd30a72d69ca
Hanae El Mansouri <43448355+crownednene@users.noreply.github.com>
2021-07-15 22:36:20 -0700

    last stable copy

--- edit_picture.py
+++ edit_picture.py
@@ -1,29 +1,91 @@
 # editpicture.py
-from google_images_search import GoogleImagesSearch
 
 from PIL import Image, Image, ImageDraw, ImageFont
-import os
-
+import os, sys, random
 from dotenv import load_dotenv
 
+from imgurpython import ImgurClient
+import imgur_downloader
+#from google_images_search import GoogleImagesSearch
+
 load_dotenv()
-DEV_KEY = os.getenv('GOOGLE_DEV_API_KEY')
-PROJECT_KEY = os.getenv('GOOGLE_PROJECT_KEY')
 
-gis = GoogleImagesSearch(DEV_KEY, PROJECT_KEY)
+CLIENT_ID = os.getenv('IMGUR_CLIENT_ID')
+CLIENT_SECRET = os.getenv('IMGUR_CLIENT_SECRET')
+ACCESS_TOKEN = os.getenv('IMGUR_ACCESS_TOKEN')
+REFRESH_TOKEN =os.getenv('IMGUR_REFRESH_TOKEN')
+imgur_client = ImgurClient(CLIENT_ID, CLIENT_SECRET, ACCESS_TOKEN, REFRESH_TOKEN)
 
+path = os.getcwd()
 
-# define search params:
+#DEV_KEY = os.getenv('GOOGLE_DEV_API_KEY')
+#PROJECT_KEY = os.getenv('GOOGLE_CX_KEY')
+#gis = GoogleImagesSearch(DEV_KEY, PROJECT_KEY)
+
+#define search params (for google custom search):
+#_search_params = {
+#	'q': 'pete buttigieg',
+#	'fileType': 'jpg|gif|png'
+#}
 
 
 def find_image():
-	gis.search(search_params={'q': 'dog'})
+	os.chdir(path + '/imageDownload/image')
+	clear_folder()
+
+#	the google custom image search way of doing it
+#	gis.search(search_params=_search_params, path_to_dir = path + '/imageDownload/image', width=500, height=500)
+
+#	i = 1
+#	for file in os.listdir():
+#		src = file
+#		dst = "image.png"
+#		os.rename(src,dst)
+#		i+=1
+	
+#	the imgur api way of doing it
+	items = imgur_client.gallery_search('#'+'petebuttigieg', advanced=None, sort='time', window='all', page=0)
+
+	i=1
+	for item in items:
+		if i < 3:
+			imgur_downloader.ImgurDownloader(item.link, path + '/imageDownload/image', 'image'+ str(i), True).save_images()
+			i+=1
+		else:
+			break
+
+
+def clear_folder():
+	os.chdir(path + '/imageDownload/image')
+	i = 1
+	for file in os.listdir(): 
+		if file.startswith('image'):
+			os.remove(file)
+			i+=1
+
 
 def write_blank():
 	im = Image.new('RGBA', (800, 800), 'white')
 	draw = ImageDraw.Draw(im)
 	fontsFolder = 'Library/Fonts'
-	comicSansFont = ImageFont.truetype(os.path.join(fontsFolder, 'arial.ttf'), 32)
+	comicSansFont = ImageFont.truetype(os.path.join(fontsFolder, 'comicz.ttf'), 50)
 
-	draw.text((100, 150), picTex, fill='gray', font=comicSansFont)
+	draw.text((320, 350), 'Nudes', fill=(0, 0, 0), font=comicSansFont)
 	im.save('text.png')
+
+def quinns_birthday():
+	k = random.randrange(21)
+	randomButt = str(k) + '.jpg'
+
+	r = random.randrange(256)
+	g = random.randrange(256)
+	b = random.randrange(256)
+
+	os.chdir(path + '/imageDownload/buttigieg')
+	hbButt = Image.open(randomButt)
+	draw = ImageDraw.Draw(hbButt)
+	fontsFolder = 'Library/Fonts'
+	comicSansFont = ImageFont.truetype(os.path.join(fontsFolder, 'comicz.ttf'), 50)
+
+	draw.text((100, 100), "HAPPY BIRTHDAY QUINN", fill=(r, g, b), font=comicSansFont)
+	hbButt.save('generatedButt.jpg')
--- memey_boi.py
+++ memey_boi.py
@@ -1,21 +1,29 @@
 # memey_boi.py
+
 import os
+import discord
 import edit_picture
 
-import discord
-
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
+path = os.getcwd()
+
+#define the command prefix
+bot = commands.Bot(command_prefix='$')
+
+
 
 #check for event
 @client.event
 #makes the bot online on discord
 async def on_ready():
 	print('We have logged in as {0.user}.'.format(client))
+
 
 #check for event
 @client.event
@@ -29,9 +37,9 @@ async def on_message(message):
 		return
 
 	#if someone types $hello, reply with "fuck off"
-	if message.content.startswith('$hello'):
+	if message.content.startswith('$hi'):
 		await message.channel.send('fuck off')
-		await client.change_presence(status=discord.Status.idle, activity=game)
+		await client.change_presence(status=discord.Status.idle, activity="you knoooww...")
 
 	if "anata wa botto desu ka?" == message.content.lower():
 		await message.channel.send('はい')
@@ -39,16 +47,37 @@ async def on_message(message):
 	if "send nudes" == message.content.lower():
 		await client.change_presence(status=discord.Status.online, activity=discord.Game("with nudes"))
 		channel = client.get_channel(message.channel.id)
-		print(f"{channel}")
+		edit_picture.write_blank()
 		await channel.send(file=discord.File('text.png'))
 
 	#if someone types $byebitch, reply with "wtvr loser" and disconnect
-	if "$byebitch" == message.content.lower():
+	if "bitch bye" == message.content.lower():
 		await message.channel.send('wtvr loser')
 		await client.close()
 
 	if "what" == message.content.lower():
+		channel = client.get_channel(message.channel.id)
 		edit_picture.find_image()
-		await channel.send()
+		await message.channel.send('found dog')
+		#await channel.send()
+
+	if "happy birthday" in message.content.lower() or "hap birth" in message.content.lower():
+		await client.change_presence(status=discord.Status.online, activity=discord.Game("birthday wishes"))
+		channel = client.get_channel(message.channel.id)
+		edit_picture.quinns_birthday()
+		await channel.send(file=discord.File(path + '/imageDownload/buttigieg/generatedButt.jpg'))
 
 client.run(TOKEN)
+
+
+@bot.command()
+async def test(ctx, *args):
+	await ctx.send(format(len(args), ' '.join(args)))
+
+async def hello(ctx, arg):
+	await ctx.send("sup dude")
+
+async def bye(ctx, arg):
+	bot.close()
+
+bot.run(TOKEN)
